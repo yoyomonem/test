6b6f0a3a0c76c7cf2a6861da96a4608ad44920dd
Youssef <yoyo.monem22@gmail.com>
2021-12-03 18:26:55 +0100

    Create ms.py

---
+++ ms.py
@@ -0,0 +1,23 @@
+import time
+from random import randint
+def tech(trigger):
+    print("We are Microsoft. We make technology.")
+def make(obj_c):
+    answer = input("Objective-C is good. Do you want to make it? (Y/n) ")
+    if answer = "Y" or answer = "y":
+        print("Good choice. Let's get going to Make.")
+        time.sleep(2)
+        print("First, let's use the BSoD's physical memory dump.")
+        time.sleep(2)
+        print("Beginning dump of physical memory...")
+        time.sleep(3)
+        print("Physical memory dump complete.")
+        print("Then we begin to \"Make\".")
+        time.sleep(3)
+    elif answer = "n" or answer = "N":
+        print("Oh. So you don't want to do so.")
+    else:
+        print("Wait, what?!")
+        time.sleep(2)
+        print("This is an incorrect answer!")
+        print("You're banned from here!")

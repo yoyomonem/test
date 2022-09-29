c78c5b6cbeca0e590756d954c0d116c14c9174ef
Y Nasr <youssef.land@outlook.com>
2022-09-29 19:27:19 +0200

    Update ms.py

--- ms.py
+++ ms.py
@@ -2,7 +2,7 @@ import time
 from random import randint
 def tech(trigger):
     print("We are Microsoft. We make technology.")
-def make(obj_c):
+def make(obj_c, see_if_is_found):
     answer = input("Objective-C is good. Do you want to make it? (Y/n) ")
     if answer = "Y" or answer = "y":
         print("Good choice. Let's get going to Make.")
@@ -14,11 +14,6 @@ def make(obj_c):
         print("Physical memory dump complete.")
         print("Then we begin to \"Make\".")
         time.sleep(3)
-    elif answer = "n" or answer = "N":
-        print("Oh. So you don't want to do so.")
     else:
-        print("Wait, what?!")
-        time.sleep(2)
-        print("This is an incorrect answer!")
-        print("You're banned from here!")
-make(obj_c="yes")
+        print("Abort.")
+make("yes", "yes")
